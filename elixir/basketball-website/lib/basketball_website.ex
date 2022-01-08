2f1d5754cd8eddc8186ec740a4a3057d7abad8be
joabehenrique <henriquecidoz@hotmail.com>
2022-01-08 16:17:22 -0300

    feat: nova atividade adicionada [finalizada]

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,9 +1,7 @@
 defmodule BasketballWebsite do
-  def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
-  end
+  def extract_from_path(data, path),
+    do: Enum.reduce(String.split(path, "."), data, fn valor, data -> data[valor] end)
 
-  def get_in_path(data, path) do
-    # Please implement the get_in_path/2 function
-  end
+  def get_in_path(data, path),
+    do: data |> Kernel.get_in(String.split(path, "."))
 end
